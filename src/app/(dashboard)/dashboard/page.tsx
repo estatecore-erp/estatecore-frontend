4cd5a6052fc7880091f0753cb73cdd0f9c4e9882
Sasmitha Ashinsana <sinhalayapage3@gmail.com>
2026-06-24 00:45:50 +0530

    feat: add logos

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,8 +1,17 @@
 import LogoutButton from "@/components/auth/LogoutButton";
 
+import Logo from "@/../public/logo.svg";
+import LogoWide from "@/../public/logo-wide.svg";
+import Image from "next/image";
+
 const DashboardPage = () => {
   return (
     <div>
+
+      {/* set size 45 for sidebar/navbar */}
+      
+      <Image src={Logo} alt="logo" width={85} height={85} />
+      <Image src={LogoWide} alt="logo" height={85} />
       <p>DashboardPage</p>
       <LogoutButton />
     </div>
