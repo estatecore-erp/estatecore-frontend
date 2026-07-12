d4fe7ac2e248901113d6d68e296d223b63b20bd8
Sasmitha Ashinsana <sinhalayapage3@gmail.com>
2026-07-13 00:55:51 +0530

    refactor: register form width

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -3,7 +3,7 @@ import RegisterForm from "@/components/auth/RegisterForm";
 const RegisterPage = () => {
   return (
     <div className="min-h-screen flex items-center justify-center px-4">
-      <div className="max-w-md mx-auto w-full">
+      <div className="max-w-sm mx-auto w-full">
         <RegisterForm />
       </div>
     </div>
